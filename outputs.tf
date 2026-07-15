b0ec7a3bc789adfa30547bb7f9f6b883e9ee9038
tusharsabale <109800678+tusharsabale@users.noreply.github.com>
2026-07-15 23:32:18 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "instance_public_ip" {
+/*output "instance_public_ip" {
   value = aws_instance.web-2.public_ip
+}*/
+
+output "instance_public_ip" {
+  value = aws_instance.web-2[*].public_ip
 }
