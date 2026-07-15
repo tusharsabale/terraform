13e534792ff87fa3c8e631d7ced0b031f333169b
tusharsabale <109800678+tusharsabale@users.noreply.github.com>
2026-07-15 23:31:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-resource "aws_instance" "web-2" {
+/*resource "aws_instance" "web-2" {
   ami           = var.ami
   instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
+  }
+}*/
+
+resource "aws_instance" "web-2" {
+  count = 3
+  
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "${var.instance_name}-${count.index + 1}"
   }
 }
