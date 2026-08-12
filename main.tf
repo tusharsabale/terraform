16c51c7e4542409ea4e7455c4db547e93acefecf
tushar <sabaletushar98@gmail.com>
2026-08-12 15:20:03 +0530

    Initial commit: EC2 + security group with variables

--- main.tf
+++ main.tf
@@ -1,4 +1,62 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_instance" "web" {
-  ami           = var.ami
+  ami           = var.ami_id
   instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "terraform-web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,29 @@
-variable "instance_type" {
-  type    = string
-  default = "t2.micro"
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "ami" {
-  type    = string
-  default = "ami-01b799c439fd5516a"
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+  default     = "ami-0f8a61b66d1accaee"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name"
+  type        = string
+  default     = "devops-key"
+}
+
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
+  default     = "terraform-web-server"
 }
